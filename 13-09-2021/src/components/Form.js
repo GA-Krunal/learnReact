bd3e060ae9ba11a9c1d4ef6e5296eec28177e511
krunal <krunal.patel@green-apex.com>
2021-09-14 14:59:20 +0530

    final

--- 13-09-2021/src/components/Form.js
+++ 13-09-2021/src/components/Form.js
@@ -3,6 +3,7 @@ import { Button, Grid } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField';
 
 
+
 export default class Form extends Component {
 
     render() {
@@ -11,7 +12,7 @@ export default class Form extends Component {
 
             <Grid container direction="column" alignItems="center" justify="center">
                 <form onSubmit={updateItems}>
-                    <TextField id="outlined-basic" label="enter task name here" variant="outlined" value={val} onChange={updateValue} />
+                    <TextField id="outlined-basic" label="enter task name here" color ="primary" variant="outlined" value={val} onChange={updateValue} style={{ marginTop: "10px" }}/>
                     <Grid container direction="column" alignItems="center" justify="center" >
                         <Button color="primary" variant="contained" type="submit" style={{ marginTop: "20px" }}>Add item</Button>
                     </Grid>
